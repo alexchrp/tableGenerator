fe1468f082d1cb7e29525bda25d80969e4c9bcab
alexchrp <illest.gt@gmail.com>
2017-11-17 21:55:22 +0500

    Add horizontalAlign field to Cell class

--- src/main/java/com/alexchrp/tablegenerator/Cell.java
+++ src/main/java/com/alexchrp/tablegenerator/Cell.java
@@ -1,15 +1,33 @@
 package com.alexchrp.tablegenerator;
 
+import com.alexchrp.tablegenerator.aligns.HorizontalAlign;
+
 public class Cell {
 
     public static Cell of(String text) {
         return new Cell(text);
     }
-    
+
+    public static Cell of(String text, HorizontalAlign horizontalAlign) {
+        return new Cell(text, horizontalAlign);
+    }
+
     private final String text;
+
+    private final HorizontalAlign horizontalAlign;
 
     private Cell(String text) {
         this.text = text;
+        this.horizontalAlign = null;
+    }
+
+    private Cell(String text, HorizontalAlign horizontalAlign) {
+        this.text = text;
+        this.horizontalAlign = horizontalAlign;
+    }
+
+    public HorizontalAlign getHorizontalAlign() {
+        return horizontalAlign;
     }
 
     public String getText() {
@@ -20,6 +38,7 @@ public class Cell {
     public String toString() {
         return "Cell{" +
                 "text='" + text + '\'' +
+                ", horizontalAlign=" + horizontalAlign +
                 '}';
     }
 }
