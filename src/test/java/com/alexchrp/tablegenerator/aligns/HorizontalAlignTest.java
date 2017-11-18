2fa4c43bf0a527d01376a7b974b345a76dc5d5d7
alexchrp <illest.gt@gmail.com>
2017-11-18 19:48:12 +0500

    Reformat tests, add additional tests

--- src/test/java/com/alexchrp/tablegenerator/aligns/HorizontalAlignTest.java
+++ src/test/java/com/alexchrp/tablegenerator/aligns/HorizontalAlignTest.java
@@ -1,41 +1,96 @@
 package com.alexchrp.tablegenerator.aligns;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HorizontalAlignTest {
+    
+    private static final String TEST_STRING = "testString";
 
-    @org.junit.jupiter.api.Test
-    void leftAlignTest() {
-        String testString = "testString";
-        final int stringSize = 15;
-        String leftAlign = HorizontalAlign.LEFT.apply(testString, stringSize);
-        assertEquals(stringSize, leftAlign.length());
-        final int spacesBeginIndex = testString.length();
+    private static final int STRING_SIZE = 15;
+    
+    private static String leftAlign = HorizontalAlign.LEFT.apply(TEST_STRING, STRING_SIZE);
+    private static String centerAlign = HorizontalAlign.CENTER.apply(TEST_STRING, STRING_SIZE);
+    private static String rightAlign = HorizontalAlign.RIGHT.apply(TEST_STRING, STRING_SIZE);
+    
+    @Test
+    void leftAlignLengthTest() {
+        assertEquals(STRING_SIZE, leftAlign.length());
+    }
+
+    @Test
+    void leftAlignSpacesCountTest() {
+        final int spacesBeginIndex = TEST_STRING.length();
         assertEquals("     ", leftAlign.substring(spacesBeginIndex));
     }
-
-    @org.junit.jupiter.api.Test
-    void centerAlignTest() {
-        String testString = "testString";
-        final int stringSize = 15;
-        String centerAlign = HorizontalAlign.CENTER.apply(testString, stringSize);
-        assertEquals(stringSize, centerAlign.length());
-
-        final int spaceEndIndex = (stringSize - testString.length()) / 2;
-        final int spacesBeginIndex = 0;
-        assertEquals("  ", centerAlign.substring(spacesBeginIndex, spaceEndIndex));
-
-        final int spacesBeginIndex2 = spaceEndIndex + testString.length();
-        assertEquals("   ", centerAlign.substring(spacesBeginIndex2));
+    
+    @Test
+    void centerAlignLengthTest() {
+        assertEquals(STRING_SIZE, centerAlign.length());
     }
 
-    @org.junit.jupiter.api.Test
-    void rightAlignTest() {
-        String testString = "testString";
-        final int stringSize = 15;
-        String rightAlign = HorizontalAlign.RIGHT.apply(testString, stringSize);
-        assertEquals(stringSize, rightAlign.length());
-        final int spacesEndIndex = stringSize - testString.length();
+    @Test
+    void centerAlignStartSpacesCountTest() {
+        final int spaceEndIndex = (STRING_SIZE - TEST_STRING.length()) / 2;
+        final int spacesBeginIndex = spaceEndIndex + TEST_STRING.length();
+        assertEquals("   ", centerAlign.substring(spacesBeginIndex));
+    }
+
+    @Test
+    void centerAlignEndSpacesCountTest() {
+        final int spaceEndIndex = (STRING_SIZE - TEST_STRING.length()) / 2;
+        final int spacesBeginIndex = 0;
+        assertEquals("  ", centerAlign.substring(spacesBeginIndex, spaceEndIndex));
+    }
+
+    @Test
+    void rightAlignLengthTest() {
+        assertEquals(STRING_SIZE, rightAlign.length());
+    }
+
+    @Test
+    void centerAlignSpacesCountTest() {
+        final int spacesEndIndex = STRING_SIZE - TEST_STRING.length();
         assertEquals("     ", rightAlign.substring(0, spacesEndIndex));
+    }
+
+    @Test
+    void leftAlignEmptyStringTest() {
+        final String leftAlign = HorizontalAlign.LEFT.apply("", STRING_SIZE);
+        assertEquals(15, leftAlign.length());
+    }
+
+    @Test
+    void centerAlignEmptyStringTest() {
+        final String centerAlign = HorizontalAlign.CENTER.apply("", STRING_SIZE);
+        assertEquals(15, centerAlign.length());
+    }
+
+    @Test
+    void rightAlignEmptyStringTest() {
+        final String rightAlign = HorizontalAlign.RIGHT.apply("", STRING_SIZE);
+        assertEquals(15, rightAlign.length());
+    }
+
+    @Test
+    void leftAlignStringLongerThanAlignTest() {
+        final String testString = "testStringTestString";
+        final String leftAlign = HorizontalAlign.LEFT.apply(testString, STRING_SIZE);
+        assertEquals(testString, leftAlign);
+    }
+
+    @Test
+    void centerAlignStringLongerThanAlignTest() {
+        final String testString = "testStringTestString";
+        final String centerAlign = HorizontalAlign.CENTER.apply(testString, STRING_SIZE);
+        assertEquals(testString, centerAlign);
+    }
+
+    @Test
+    void rightAlignStringLongerThanAlignTest() {
+        final String testString = "testStringTestString";
+        final String rightAlign = HorizontalAlign.RIGHT.apply(testString, STRING_SIZE);
+        assertEquals(testString, rightAlign);
     }
 }
