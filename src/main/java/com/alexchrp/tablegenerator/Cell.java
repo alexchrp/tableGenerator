f4da7e30c4b979c065e1b2c7c81bea9576b3ba70
alexchrp <illest.gt@gmail.com>
2017-11-18 07:46:23 +0500

    Add method getVerticalAlign to class Cell

--- src/main/java/com/alexchrp/tablegenerator/Cell.java
+++ src/main/java/com/alexchrp/tablegenerator/Cell.java
@@ -55,6 +55,10 @@ public class Cell {
         return horizontalAlign;
     }
 
+    public VerticalAlign getVerticalAlign() {
+        return verticalAlign;
+    }
+
     public String getText() {
         return text;
     }
