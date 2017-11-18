a6844628778f013d194680ee76aa879dca110d2b
alexchrp <illest.gt@gmail.com>
2017-11-18 07:30:30 +0500

    Add class Row

---
+++ src/main/java/com/alexchrp/tablegenerator/Row.java
@@ -0,0 +1,34 @@
+package com.alexchrp.tablegenerator;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Row {
+
+    private final List<Cell> cells;
+
+    public Row(List<Cell> cells) {
+        this.cells = cells;
+    }
+
+    public Row addCell(String text) {
+        cells.add(Cell.of(text));
+        return this;
+    }
+
+    public Row addCell(Cell cell) {
+        cells.add(cell);
+        return this;
+    }
+
+    public List<Cell> getCells() {
+        return Collections.unmodifiableList(cells);
+    }
+
+    @Override
+    public String toString() {
+        return "Row{" +
+                "cells=" + cells +
+                '}';
+    }
+}
